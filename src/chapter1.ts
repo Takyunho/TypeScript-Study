24dea10479a23d26853581becaa20c0bdd9294f5
Yunho <tyh1819@gmail.com>
2026-02-04 13:15:36 +0900

    add: 원시타입과 리터럴 타입

---
+++ src/chapter1.ts
@@ -0,0 +1,31 @@
+// number type
+// : number처럼 타입을 명시해주는 것을 타입 주석 또는 타입 어노테이션이라고 함
+let number1: number = 10;
+let number2: number = -10;
+let number3: number = 1.1;
+let number4: number = -1.1;
+let number5: number = Infinity;
+let number6: number = -Infinity;
+let number7: number = NaN;
+
+// string type
+let string1: string = "Hello World";
+let string2: string = "Hello World";
+let string3: string = `Hello World`;
+let string4: string = `Hello ${string1}`;
+
+// boolean type
+let boolean1: boolean = true;
+let boolean2: boolean = false;
+
+// null type
+let null1: null = null;
+
+// undefined type
+let undefined1: undefined = undefined;
+
+// 리터럴 타입
+// 리터럴 -> 값
+let numA: 10 = 10;
+let strA: "Hello" = "Hello";
+let boolA: true = true;
